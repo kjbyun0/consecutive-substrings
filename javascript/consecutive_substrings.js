fbfa90023c10bbffed5b63cca5f2235d08f305bf
KI JONG BYUN <kjbyun0@gmail.com>
2024-08-27 16:30:11 -0500

    Add my solution

--- javascript/consecutive_substrings.js
+++ javascript/consecutive_substrings.js
@@ -1,5 +1,12 @@
 function consecutiveSubstrings(string) {
   // type your code here
+  const res = [];
+  for (let i = 0; i < string.length; i++) {
+    for (let j = i; j < string.length; j++) {
+      res.push(string.slice(i, j+1));
+    }
+  }
+  return res;
 }
 
 if (require.main === module) {
@@ -11,6 +18,14 @@ if (require.main === module) {
 
   console.log("Expecting: ['a']");
   console.log("=>", consecutiveSubstrings('a'));
+
+  console.log('');
+  console.log("Expecting: []");
+  console.log("=>", consecutiveSubstrings(''));
+
+  console.log('');
+  console.log("Expecting: [a, ab]");
+  console.log("=>", consecutiveSubstrings('ab'));
 }
 
 module.exports = consecutiveSubstrings;
